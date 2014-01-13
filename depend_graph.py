463bbcde31265d6e96d267b494215b93e8fe3b11
Doug Hellmann <doug@doughellmann.com>
2014-01-13 18:32:24 -0500

    include subgraphs (though they don't show in output)

--- depend_graph.py
+++ depend_graph.py
@@ -9,14 +9,17 @@ import fileinput
 def get_deps_by_module(infile):
     library_name = None
     dependencies = {}
+    libraries = {}
     for n, line in enumerate(fileinput.input(infile)):
         if line.startswith('== '):
             # New lib
             library_name = line.strip().strip('=').strip()
+            libraries[library_name] = []
             module_name = None
         elif line.startswith('=== '):
             # New module
             module_name = line.strip().strip('=').strip()
+            libraries[library_name].append(module_name)
             dependencies[module_name] = []
         elif line.startswith(':Depends on:'):
             # Dependencies
@@ -26,39 +29,49 @@ def get_deps_by_module(infile):
             if depstr != '(none)':
                 deps = [d.strip() for d in depstr.split(',')]
                 dependencies[module_name] = deps
-    return dependencies
+    return libraries, dependencies
 
-def print_graph(dependencies):
-    node_namer = iter('N%d' % i for i in itertools.count())
-    names = collections.defaultdict(node_namer.next)
+    
+_node_namer = iter('N%d' % i for i in itertools.count())
+_node_names = collections.defaultdict(_node_namer.next)
+_known_nodes = set()
+def generate_node(label):
+    if label not in _known_nodes:
+        print '  %s [ label = "%s" ];' % (_node_names[label], label)
+        _known_nodes.add(label)
+
+    
+def print_graph(libraries, dependencies):
 
     print 'digraph oslo {'
 
-    nodes = set()
+    # Each library is a subgraph
+    for name, modules in sorted(libraries.items()):
+        n = _node_namer.next()
+        print 'subgraph %s {' % n
+        print '  label = "%s";' % name
+        print '  style = filled;'
+        print '  color=black;'
+        print '  node [style=filled,color=lightgrey];'
+        for mod in modules:
+            generate_node(mod)
+        print '}'
 
     # Generate node label statements
     for mod, deps in sorted(dependencies.items()):
-        if mod not in nodes:
-            print '  %s [ label = "%s" ];' % (names[mod], mod)
-            nodes.add(mod)
+        generate_node(mod)
         for d in deps:
-            if d not in nodes:
-                print '  %s [ label = "%s" ];' % (names[d], d)
-                nodes.add(d)
+            generate_node(d)
                 
     print
 
     # Generate the edges
     for mod, deps in sorted(dependencies.items()):
-        if mod not in names:
-            names[mod] = node_namer.next()
         for d in deps:
-            if d not in names:
-                names[d] = node_namer.next()
-            print '  %s -> %s;' % (names[mod], names[d])
+            print '  %s -> %s;' % (_node_names[mod], _node_names[d])
 
     print '}'
 
 
-dependencies = get_deps_by_module('status.txt')
-print_graph(dependencies)
+libraries, dependencies = get_deps_by_module('status.txt')
+print_graph(libraries, dependencies)
--- make_image.sh
+++ make_image.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
+set -ex
+
 depend_graph.py > graph.dot
 dot -Tpng -ograph.png graph.dot
+neato -Tpng -oneato.png graph.dot
+twopi -Tpng -otwopi.png graph.dot
+circo -Tpng -ocirco.png graph.dot
