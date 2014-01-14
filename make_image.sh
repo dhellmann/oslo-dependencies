dc4da79b3574bb579da1016eebc91315e3838d18
Doug Hellmann <doug@doughellmann.com>
2014-01-14 09:33:19 -0500

    only draw the images that are actually useful

--- make_image.sh
+++ make_image.sh
@@ -5,9 +5,9 @@ set -ex
 function draw {
     typeset base=$1
     dot -Tpng -o${base}.png ${base}.dot
-    neato -Tpng -o${base}_neato.png ${base}.dot
-    twopi -Tpng -o${base}_twopi.png ${base}.dot
-    circo -Tpng -o${base}_circo.png ${base}.dot
+#    neato -Tpng -o${base}_neato.png ${base}.dot
+#    twopi -Tpng -o${base}_twopi.png ${base}.dot
+#    circo -Tpng -o${base}_circo.png ${base}.dot
 }
 
 depend_graph.py > modules.dot
